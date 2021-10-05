72d1ed3c409c0a6609adaa493140aa5672f3a553
Rudrrayan manna <rudrrayan@gmail.com>
2021-10-05 21:10:20 +0100

    some code
    added decryptor class and some basic cli

---
+++ decryptor.py
@@ -0,0 +1,32 @@
+# decrypts stuff in one nice class package
+import time # timing
+
+class Decryptor():
+    topwords = []
+    code = ""
+    
+
+    def __init__(self, code):
+        x = time.time()
+        print("initialising...")
+        self.code = code
+        print("reading commonwords")
+        with open("commonwords","r") as f:
+            for line in f.readlines():
+                self.topwords.append(line[:-1]) # [:-1] because of \n at end of line
+        print("initilised!")
+        print(f"{time.time()-x}")
+        pass
+
+    def cli(self):
+        print("Welcome to the CLI")
+        print("What would you like to do? (type H for help)")
+        print("\033[F"*1000)
+        
+        command = input()
+
+
+    # rates how close the text is to "english"
+    def englishity(self) -> float:
+
+        return 0
---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from decryptor import Decryptor
+
+main = Decryptor("hlasjoaisjdoiajd")
+
+while True:
+    main.cli()
